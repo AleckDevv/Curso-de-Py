066e647fbdfd181d4a9f68198c9ac80dfe7d2704
Alex Soares Lopes <alex.lopes@somosicev.com>
2025-07-23 18:46:48 -0300

    enviado arquivo de loços encadeados

---
+++ lacos_encadeados.py
@@ -0,0 +1,9 @@
+for cont_ex in range(1, 5):
+    print(f"Rodada: {cont_ex}")
+    # roda o print do 1 ao 4
+
+    for cont_int in range(5, 0, -1):
+        print(f"valor: {cont_int}")
+        # roda o print de forma regresiva
+        # do 5 ao 0 subtraindo -1 desse valor
+print("fim")
