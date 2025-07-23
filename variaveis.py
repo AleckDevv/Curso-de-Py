045914895f9504ec01a9cd0cc9779a2e1f866b85
Alex Soares Lopes <as3434126@gmail.com>
2025-07-23 09:51:51 -0300

    curso de py

---
+++ variaveis.py
@@ -0,0 +1,14 @@
+nome = "Alex"
+idade = 25
+verdadeiro = True
+falso = False
+flutuante = 1.2
+array = [10, "teste", True]
+objeto = {nome: "Alex", idade: 25}
+
+print(type(nome))
+""" 
+função type() para saber o tipo de dados
+ """
+print(array)
+
