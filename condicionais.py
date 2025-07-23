6dd567f540708b8f9ce62f5dab3c5106e7bf3a03
Alex Soares Lopes <alex.lopes@somosicev.com>
2025-07-23 15:12:17 -0300

    add aquivo sobre condicionais

---
+++ condicionais.py
@@ -0,0 +1,25 @@
+"""n1 = n2 = 0
+media = 0.0
+
+n1 = float(input("Digite uma nota: "))
+n2 = float(input("Digite a segunda nota: "))
+
+media = (n1 + n2) / 2
+
+if media >= 7:
+    print("aprovado")
+elif media(media >= 5):
+    print("recuperação")
+else:
+    print("reprovado")
+"""
+
+n1 = 10
+n2 = 20
+
+if n1 > n2:
+    print(f"{n1} é maior")
+elif n2 > n1:
+    print(f"{n2} é maior")
+else:
+    print("valor inválido")
---
+++ operadores_logicos.py
@@ -0,0 +1,34 @@
+"""
+operador and = ambas tem que ser verdadeira
+"""
+
+""" idade = 25
+altura = 1.75
+
+resultado = (idade >= 18) and (altura >= 1.70)
+print(f"Pode participar: {resultado}");
+ """
+
+""" 
+operador or
+basta uma condição ser ok para que seja verdadeira
+"""
+""" porta = "a"
+janela = "f"
+
+alarme = (porta == "a") or (janela == "a")
+print(f"Disparado? {alarme}")
+
+ """
+
+""" 
+operador not
+inverte o estado lógico
+Se é verdadeiro passa a ser falso
+Se é falso passa a ser verdadeiro
+"""
+
+tem_dinheiro = False
+tem_dinheiro = not tem_dinheiro
+print(f"Tem dinheiro: {tem_dinheiro}")
+
