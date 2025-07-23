eaeeb7328d08b71830733a0ef0cc6f822fd5536c
Alex Soares Lopes <as3434126@gmail.com>
2025-07-23 10:48:35 -0300

    enviando arquivos sobre operadores aritiméticos em py

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,68 @@
+"""
+Operadores:
+
++ -> soma
+- -> subtração
+* -> multiplicação
+/ -> divisão
+// -> divisão inteira
+% -> resto da divisão
+** -> exponenciação
+"""
+
+# soma
+""" 
+a = 10
+b = 18
+soma = a + b
+print(soma) 
+"""
+
+# subtração
+""" 
+a = 10
+b = 5
+subtracao = a - b
+print(subtracao)
+"""
+
+# multiplicação
+""" 
+a = 10
+b = 10
+multiplicacao = a * b
+print(f"Mulplicação {multiplicacao}")
+ """
+
+# Divisão
+""" 
+a = 10
+b = 10
+divisao = a / b
+print(f"Divisão: {divisao}") 
+"""
+
+# Divisão inteira
+""" 
+a = 10
+b = 10
+divisao_inteira = a // b
+print(f"Divisão inteira: {divisao_inteira}") """
+
+# Resto da divisão
+""" 
+a = 10
+b = 10
+resto = a % b
+print(f"Resto: {resto}") 
+"""
+
+
+# Exponenciação
+""" 
+a = 10
+b = 10
+exponenciacao = a ** b
+print(f"Ao quadrado: {exponenciacao}") 
+"""
+
--- variaveis.py
+++ variaveis.py
@@ -1,3 +1,4 @@
+# variáveis e tipos de dados
 nome = "Alex"
 idade = 25
 verdadeiro = True
@@ -12,3 +13,50 @@ função type() para saber o tipo de dados
  """
 print(array)
 
+""" 
+isinstance() verifica se determinada veriável
+é de tal tipo de dados
+ """
+print(isinstance(idade, int))
+print(isinstance(nome, int))
+""" 
+tem como verificar mais de um tipo em uma condição
+ """
+print(isinstance(idade, (int, float)))
+
+
+nome2 = "Alex Soares"
+idade2 = 18
+altura = 1.75
+tem_carteira = True
+
+""" 
+template string em py
+ """
+print(f"tetse: {nome2}")
+
+# str
+nome3 = "pedro"
+
+# int
+ano = 2025
+
+# float
+preco = 9.99
+
+# booleano
+tem_carteira = True
+esta_chovendo = False
+
+# array
+cidades = ["São Paulo", "Rio", "Belo Horizonte", 10, True]
+
+# objeto
+usuario = {
+    "nome": "Alex",
+    "idade": 24,
+    "email": "alex@gmail.com"
+}
+
+# none
+vazio = None
